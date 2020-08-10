0340543fe6dcfc67a5d0dba695a51c610e3d1da0
shigasy <kxx555u.u@gmail.com>
2020-08-11 01:50:16 +0900

    add: timerモデルとViewControllerを紐付け

--- EggTimer/ViewControllers/ViewController.swift
+++ EggTimer/ViewControllers/ViewController.swift
@@ -9,34 +9,44 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet weak var timeLeftField: NSTextField!
     @IBOutlet weak var eggImageView: NSImageView!
     @IBOutlet weak var startButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var resetButton: NSButton!
+    var eggTimer = EggTimer()
+    
     
     @IBOutlet weak var www: NSButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        eggTimer.delegate = self
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
     @IBAction func startButtonClicked(_ sender: Any) {
-        print("a")
+        if eggTimer.isPaused {
+            eggTimer.resumeTimer()
+        } else {
+            eggTimer.duration = 360
+            eggTimer.startTimer()
+        }
     }
     @IBAction func stopButtonClicked(_ sender: Any) {
+        eggTimer.stopTimer()
     }
     @IBAction func resetButtonClicked(_ sender: Any) {
+        eggTimer.resetTimer()
+        updateDisplay(for: 360)
     }
     // MARK: - IBActions - menus
-
+    
     @IBAction func startTimerMenuItemSelected(_ sender: Any) {
         startButtonClicked(sender)
     }
@@ -48,3 +58,57 @@ class ViewController: NSViewController {
     }
 }
 
+extension ViewController: EggTimerProtocol {
+    
+    func updateDisplay(for timeRemaining: TimeInterval) {
+        timeLeftField.stringValue = textToDisplay(for: timeRemaining)
+        eggImageView.image = imageToDisplay(for: timeRemaining)
+    }
+    
+    private func textToDisplay(for timeRemaining: TimeInterval) -> String {
+        if timeRemaining == 0 {
+            return "Done!"
+        }
+        
+        let minutesRemaining = floor(timeRemaining / 60)
+        let secondsRemaining = timeRemaining - (minutesRemaining * 60)
+        
+        let secondsDisplay = String(format: "%02d", Int(secondsRemaining))
+        let timeRemainingDisplay = "\(Int(minutesRemaining)):\(secondsDisplay)"
+        
+        return timeRemainingDisplay
+    }
+    
+    private func imageToDisplay(for timeRemaining: TimeInterval) -> NSImage? {
+        let percentageComplete = 100 - (timeRemaining / 360 * 100)
+        
+        if eggTimer.isStopped {
+            let stoppedImageName = (timeRemaining == 0) ? "100" : "stopped"
+            return NSImage(named: stoppedImageName)
+        }
+        
+        let imageName: String
+        switch percentageComplete {
+        case 0 ..< 25:
+            imageName = "0"
+        case 25 ..< 50:
+            imageName = "25"
+        case 50 ..< 75:
+            imageName = "50"
+        case 75 ..< 100:
+            imageName = "75"
+        default:
+            imageName = "100"
+        }
+        
+        return NSImage(named: imageName)
+    }
+    
+    func timeRemainingOnTimer(_ timer: EggTimer, timeRemaining: TimeInterval) {
+        updateDisplay(for: timeRemaining)
+    }
+    func timerHasFinished(_ timer: EggTimer) {
+        updateDisplay(for: 0)
+        
+    }
+}
